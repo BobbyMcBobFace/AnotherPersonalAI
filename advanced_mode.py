580cdf7921046cbd539d6bd4adb5dfda03f6f124
Zac <153976918+BobbyMcBobFace@users.noreply.github.com>
2025-01-26 19:20:09 +0800

    Minor Changes

--- advanced_mode.py
+++ advanced_mode.py
@@ -1,16 +1,19 @@
 from groq import Groq
 from dotenv import load_dotenv
 from pathlib import Path
-import os
 from models import models
+from main import main
+import os
+
 
 load_dotenv(dotenv_path=Path('.') / '.env')
 
+# Api
+client = Groq(
+    api_key = os.getenv("GROQ_API_KEY"),
+)
+
 def advanced_mode():
-    # Api
-    client = Groq(
-        api_key = os.getenv("GROQ_API_KEY"),
-    )
 
     # Input
     models()    
@@ -20,11 +23,37 @@ def advanced_mode():
     print("Your input is:", chat_content)
     print("Your selected model is:", selected_model)
 
-    # Confirmation    
+    # Confirmation
     if input("Do you want to continue? (yes/no): ").lower() != "yes":
-        print("\nReturning to main menu...")
-        return
+        returntomain()
 
+    guard_response = True if query_guard(chat_content) and query_guard(system_content) == "safe" else False
+
+    # If the message is safe, allow selected model to respond to it
+    if guard_response == True:
+        query_llm(system_content, chat_content, selected_model)
+        print("\n")
+        returntomain()
+
+    # If the message is unsafe, error out with a generic message 
+    else:
+        raise ValueError("Sorry, I cannot process this request due to inappropriate content.")
+
+def query_guard(chat_content):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": chat_content
+            }
+        ],
+        model="llama-guard-3-8b",
+    )
+
+    return chat_completion.choices[0].message.content
+
+
+def query_llm(system_content, chat_content, selected_model):
     # Query the LLM 
     stream = client.chat.completions.create(
     messages=[
@@ -43,3 +72,10 @@ def advanced_mode():
 
     for chunk in stream:
         print(chunk.choices[0].delta.content, end="")
+
+def returntomain():
+    if input("Do you want to return to the main menu? (yes/no): ").lower() != "yes":
+        advanced_mode()
+    else:
+        print("\nReturning to main menu...")
+        main()
--- simple_mode.py
+++ simple_mode.py
@@ -1,3 +1,4 @@
+from annotated_types import T
 from groq import Groq
 from dotenv import load_dotenv
 from pathlib import Path
@@ -22,11 +23,11 @@ def simple_mode():
         returntomain()
     
     # Run the prompt through the guard first to ensure appropriate content
-    guard_response = query_guard(chat_content)
+    guard_response = True if query_guard(chat_content) == "safe" else False
     
 
     # If the message is safe, allow Llama to respond to it
-    if guard_response.lower() == "safe":
+    if guard_response == True:
         query_llm(chat_content)
         print("\n")
         returntomain()
@@ -65,6 +66,8 @@ def query_llm(chat_content):
 
     for chunk in stream:
         print(chunk.choices[0].delta.content, end="")
+
+
 
 def returntomain():
     if input("Do you want to return to the main menu? (yes/no): ").lower() != "yes":
