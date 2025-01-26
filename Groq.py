feae27ed6c95fc446461863c70bab2f19ffed5ec
Zac <153976918+BobbyMcBobFace@users.noreply.github.com>
2025-01-26 12:01:35 +0800

    Changed chat completion to stream instead of display

--- Groq.py
+++ Groq.py
@@ -2,6 +2,7 @@ from groq import Groq
 from dotenv import load_dotenv
 from pathlib import Path
 
+from httpx import stream
 import requests 
 import os
 
@@ -10,6 +11,52 @@ load_dotenv(dotenv_path=Path('.') / '.env')
 # Simple mode - Only a prompt, nothing else 
 def simple_mode():
     
+    # Api
+    client = Groq(
+        api_key = os.getenv("GROQ_API_KEY"),
+    )
+
+    # Input
+    chat_content = input("Hi, I'm Groq. How may I help you today? ")
+    print("Your input is:", chat_content)
+
+    # Confirmation    
+    if input("Do you want to continue? (yes/no): ").lower() != "yes":
+        print("Exiting...")
+        return
+
+    # Query the LLM 
+    stream = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": chat_content,
+        }
+    ],
+    model="mixtral-8x7b-32768",
+    stream=True,
+    )
+
+    for chunk in stream:
+        print(chunk.choices[0].delta.content, end="")
+
+
+def models():
+
+    api_key = os.environ.get("GROQ_API_KEY")
+    url = "https://api.groq.com/openai/v1/models"
+
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.json())
+
+def advanced_mode():
+
     # Api
     client = Groq(
         api_key = os.getenv("GROQ_API_KEY"),
@@ -38,20 +85,6 @@ def simple_mode():
     result = chat_completion.choices[0].message.content
     print(result)
 
-
-def models():
-
-    api_key = os.environ.get("GROQ_API_KEY")
-    url = "https://api.groq.com/openai/v1/models"
-
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type": "application/json"
-    }
-
-    response = requests.get(url, headers=headers)
-
-    print(response.json())
 
 def main():
 
